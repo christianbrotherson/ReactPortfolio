52c4bfa74b7fbbd5c77fabd33af4e62107aa95f7
christianbrotherson <christian.brotherson@gmail.com>
2020-12-16 20:42:16 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Christian Brotherson React Portfolio
---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import moment from "moment";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Christian Brotherson Portfolio</h1>
+      </div>
+    );
+  }
+}
