d688d76caef2ffc0179789832917abe1bbe2f944
christianbrotherson <christian.brotherson@gmail.com>
2020-12-21 10:31:51 -0700

    built out auth page, added route, and styled

--- src/components/pages/auth.js
+++ src/components/pages/auth.js
@@ -1,10 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import loginImage from '../../../static/assets/images/auth/login.jpg';
 
 export default class Auth extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="auth-page-wrapper">
+                <div 
+                    className="left-column" 
+                    style={{
+                        backgroundImage: `url(${loginImage})`
+                    }}
+                />
+                <div className="right-column">
+                    <h1>Login component goes here...</h1>
+                </div>
             </div>
         )
     }
